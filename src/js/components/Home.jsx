cb99c09cfb8efabef5574fd36e4f2519c2887b77
CarVill30 <carlosrogervillarreal3@gmail.com>
2025-03-22 05:40:37 +0000

    haciendo el proyecto

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,23 +6,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<div className="support">	
+				<div className="app d-inline-flex flex-column flex-nowrap"> 
+					<div  onClick={() => setColor("red")}    className={color === "red"    ? "red light" :     "red" }  	>		</div>
+					<div  onClick={() => setColor("yellow")} className={color === "yellow" ? "yellow light ":  "yellow" } 	>		</div>
+					<div  onClick={() => setColor("green")}  className={color === "green"  ? "green  light":	"green" } 	>		</div>
+				</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+				<div class="btn-group" role="group" aria-label="Basic example">
+
+					<button type="button" class="btn btn-primary"onClick={clic}>Cambiar Color</button>
+			
+				</div>
+		
+			</div> 
+	)
 };
 
-export default Home;
+export default Home
